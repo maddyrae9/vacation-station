ad46d4ae94ac3cd20f547a5182fe02b9413fff94
milfred08 <milfred08@gmail.com>
2022-07-20 20:13:50 -0400

    updated homeroutes

--- controllers/api/groceryStores.js
+++
@@ -1,3 +0,0 @@
-const User = require("./User");
-const GroceryStores = require("./GroceryStores");
-const homeRoutes = require('./home-routes.js');
--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,21 +2,9 @@ var express = require('express');
 var router = express.Router();
 var db = require("../models");
 
-
- router.get("/Grocery Stores/search", function (req, res) {
-
-     var type = req.query.type;  
-     var GroceryStoresSearch =req.query.GroceryStores_search; 
-     db.GroceryStores.findAll({
-         where: { [type]: GroceryStoresSearch}
-      }).then(function(GroceryStores) {
-        console.log(GroceryStores)
-        res.render("results", {GroceryStores});
-      });
-    
-     
-    });
-
+router.get('/', (req, res) => {
+  res.render('homepage');
+  });
    
     
 
