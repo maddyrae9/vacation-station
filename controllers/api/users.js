c71ef7491f010f723a2a201f1f71cd52d8119bff
milfred08 <milfred08@gmail.com>
2022-07-20 21:06:54 -0400

    updated users

--- controllers/api/users.js
+++ controllers/api/users.js
@@ -1,172 +1,160 @@
-var db = require("../models");
-var passport = require("../config");
+const router = require('express').Router();
+const { User, Post, Comment, Vote } = require('../../models');
 
-
-module.exports = function (app) {
-
-    app.post("/api/login", passport.authenticate("local"), function (req, res) {
-        res.json(req.user);
+// get all users
+router.get('/', (req, res) => {
+  User.findAll({
+    attributes: { exclude: ['password'] }
+  })
+    .then(dbUserData => res.json(dbUserData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
     });
+});
 
-    app.post("/api/signup", function (req, res) {
-        db.User.create({
-            email: req.body.email,
-            password: req.body.password
-        })
-            .then(function () {
-                res.redirect(307, "/api/login");
-            })
-            .catch(function (err) {
-                res.status(401).json(err);
-            });
-    });
-
-    app.get("/logout", function (req, res) {
-        req.logout();
-        res.redirect("/");
-    });
-
-    app.get("/api/user_data", function (req, res) {
-        if (!req.user) {
-            // The user is not logged in, send back an empty object
-            res.json({});
-        } else {
-            // Otherwise send back the user's email and id
-            // Sending back a password, even a hashed password, isn't a good idea
-            res.json({
-                email: req.user.email,
-                id: req.user.id
-            });
+router.get('/:id', (req, res) => {
+  User.findOne({
+    attributes: { exclude: ['password'] },
+    where: {
+      id: req.params.id
+    },
+    include: [
+      {
+        model: Post,
+        attributes: ['id', 'title', 'post_url', 'created_at']
+      },
+      {
+        model: Comment,
+        attributes: ['id', 'comment_text', 'created_at'],
+        include: {
+          model: Post,
+          attributes: ['title']
         }
-    });
-
-    // Get all the users
-    app.get("/api/users", function (req, res) {
-        db.User.findAll({
-            // include: [db.Shoppingcart, db.Purchase]
-            include: [{
-                model: db.Shoppingcart,
-                include: db.grocerystores
-            }, {
-                model: db.Purchase,
-                include: db.grocerystores
-            }]
-        }).then(function (dbUser) {
-            res.json(dbUser);
-        });
-    });
-
-    /* 
-    [
-        {
-            "id": 1,
-            "name": "Juan",
-            "email": "juan@email.com",
-            "password": "password",
-            "ShoppingcartId": null,
-            "PurchaseId": null,
-            "Shoppingcart": null,
-            "Purchases": []
-        },
+      },
+      {
+        model: Post,
+        attributes: ['title'],
+        through: Vote,
+        as: 'voted_posts'
+      }
     ]
-    */
-
-
-
-    // Get one user by its id
-    app.get("/api/users/:id", function (req, res) { // Missing: user and password validation
-        db.User.findOne({
-            where: {
-                id: req.params.id
-            },
-            include: [{
-                model: db.Shoppingcart,
-                include: [db.Shoppingcart_grocerystores]
-            }, {
-                model: db.Purchase,
-                include: [db.Purchase_grocerystores]
-            }]
-        }).then(function (dbUser) {
-            res.json(dbUser);
-        });
+  })
+    .then(dbUserData => {
+      if (!dbUserData) {
+        res.status(404).json({ message: 'No user found with this id' });
+        return;
+      }
+      res.json(dbUserData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
     });
+});
 
-    /* 
-    {
-        "id": 1,
-        "name": "Juan",
-        "email": "juan@email.com",
-        "password": "password",
-        "ShoppingcartId": null,
-        "PurchaseId": null,
-        "Shoppingcart": null,
-        "Purchases": []
+router.post('/', (req, res) => {
+  // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
+  User.create({
+    username: req.body.username,
+    email: req.body.email,
+    password: req.body.password
+  })
+    .then(dbUserData => {
+      req.session.save(() => {
+        req.session.user_id = dbUserData.id;
+        req.session.username = dbUserData.username;
+        req.session.loggedIn = true;
+  
+        res.json(dbUserData);
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.post('/login', (req, res) => {
+  // expects {email: 'lernantino@gmail.com', password: 'password1234'}
+  User.findOne({
+    where: {
+      email: req.body.email
     }
-    */
-
-
-
-    // Create a new user - For Sign Up
-    app.post("/api/users", function (req, res) {
-        db.User.create(req.body).then(function (dbUser) {
-            console.log('In .POST /api/users - create()');
-            console.log('req.body: ', req.body);
-            console.log('dbUser: ', dbUser);
-            res.json(dbUser);
-        });
-    });
-
-    /* 
-    {
-        "id": 4,
-        "name": "4",
-        "email": "4@email.com",
-        "password": "4",
-        "ShoppingcartId": null,
-        "PurchaseId": null,
+  }).then(dbUserData => {
+    if (!dbUserData) {
+      res.status(400).json({ message: 'No user with that email address!' });
+      return;
     }
-    */
 
+    const validPassword = dbUserData.checkPassword(req.body.password);
 
-    // Update an user by its id
-    app.put("/api/users", function (req, res) {
-        db.User.update(
-            req.body,
-            {
-                where: {
-                    id: req.body.id
-                }
-            }).then(function (dbUser) {
-                console.log('In .PUT /api/users - update()');
-                console.log('req.body: ', req.body);
-                console.log('dbUser: ', dbUser);
-                res.json(dbUser);
-            });
+    if (!validPassword) {
+      res.status(400).json({ message: 'Incorrect password!' });
+      return;
+    }
+
+    req.session.save(() => {
+      req.session.user_id = dbUserData.id;
+      req.session.username = dbUserData.username;
+      req.session.loggedIn = true;
+  
+      res.json({ user: dbUserData, message: 'You are now logged in!' });
     });
+  });
+});
 
-    /* 
-    [
-        1
-    ]
-    */
-
-
-    // Delete an user by its id
-    app.delete("/api/users/:id", function (req, res) {
-        db.User.destroy({
-            where: {
-                id: req.params.id
-            }
-        }).then(function (dbUser) {
-            console.log('In .DELETE /api/users - destroy()');
-            console.log('req.body: ', req.body);
-            console.log('dbUser: ', dbUser);
-            res.json(dbUser);
-        });
+router.post('/logout', (req, res) => {
+  if (req.session.loggedIn) {
+    req.session.destroy(() => {
+      res.status(204).end();
     });
+  }
+  else {
+    res.status(404).end();
+  }
+});
 
-    /* 
-        1
-    */
+router.put('/:id', (req, res) => {
+  // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
 
-};
+  // pass in req.body instead to only update what's passed through
+  User.update(req.body, {
+    individualHooks: true,
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(dbUserData => {
+      if (!dbUserData) {
+        res.status(404).json({ message: 'No user found with this id' });
+        return;
+      }
+      res.json(dbUserData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.delete('/:id', (req, res) => {
+  User.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(dbUserData => {
+      if (!dbUserData) {
+        res.status(404).json({ message: 'No user found with this id' });
+        return;
+      }
+      res.json(dbUserData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+module.exports = router;
